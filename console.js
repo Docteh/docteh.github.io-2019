10b7b7f7c10b78bb22645e92ec636c77c0d9fedb
Kyle K <doctor.whom@gmail.com>
2019-07-27 08:45:36 +0000

    almost works

--- console.js
+++ console.js
@@ -1,30 +1,35 @@
-(function() {
-  'use strict';
+'use strict';
 
-  hterm.defaultStorage = new lib.Storage.Local();
+//var serial = navigator.serial;
+//reader.read().then(function(fij) { asdf=fij;let textDecoder = new TextDecoder(); t.io.print(textDecoder.decode(fij.value.buffer)); });
+hterm.defaultStorage = new lib.Storage.Local();
 
-  var port;
+var port;
+var writer;
+var reader;
 
-  let textEncoder = new TextEncoder();
+let textEncoder = new TextEncoder();
 
-  let t = new hterm.Terminal();
-  t.onTerminalReady = () => {
-    console.log('Terminal ready.');
-    let io = t.io.push();
+let t = new hterm.Terminal();
+t.onTerminalReady = () => {
+console.log('Terminal ready.');
+let io = t.io.push();
 
-    io.onVTKeystroke = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
-      }
-    };
+io.onVTKeystroke = str => {
+    if (port.writable !== undefined) {
+        writer.write(textEncoder.encode(str));
+        //port.send(textEncoder.encode(str)).catch(error => {
+        //  t.io.println('Send error: ' + error);
+        //});
+    }
+};
 
     io.sendString = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
+      if (port.writable !== undefined) {
+        port.writable.write(str);
+        //port.send(textEncoder.encode(str)).catch(error => {
+        //  t.io.println('Send error: ' + error);
+        //});
       }
     };
   };
@@ -39,7 +44,7 @@
 
     function connect() {
       t.io.println('Connecting to ' + port.device_.productName + '...');
-      port.connect().then(() => {
+      port.open().then(() => {
         console.log(port);
         t.io.println('Connected.');
         connectButton.textContent = 'Disconnect';
@@ -55,21 +60,43 @@
       });
     };
 
-    connectButton.addEventListener('click', function() {
+    connectButton.addEventListener('click', async function() {
+        console.log('hello there');
+        let readLoop = () => {
+            reader.read().then(value => {
+                console.log(value);
+                let textDecoder = new TextDecoder();
+                t.io.print(textDecoder.decode(value.value.buffer)); 
+                readLoop();
+                }, error => {console.log(error);});
+        };
+        try {
+            port = await navigator.serial.requestPort({});
+            await port.open({baudrate: 115200 })
+            writer = port.writable.getWriter();
+            reader = port.readable.getReader();
+            readLoop();
+            console.log(port);
+        } catch (error) {
+            console.error(error);
+        }
+        /*
       if (port) {
         port.disconnect();
         connectButton.textContent = 'Connect';
         port = null;
       } else {
-        serial.requestPort().then(selectedPort => {
+        serial.requestPort({baudrate: 115200}).then(selectedPort => {
+console.log(selectedPort);
           port = selectedPort;
           connect();
         }).catch(error => {
           t.io.println('Connection error: ' + error);
         });
       }
+      */
     });
-
+/*
     serial.getPorts().then(ports => {
       if (ports.length == 0) {
         t.io.println('No devices found.');
@@ -78,5 +105,6 @@
         connect();
       }
     });
+*/
   });
-})();
+
