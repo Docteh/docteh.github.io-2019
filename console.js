fc4e0323acbf31994f1f0816ccacaab6b6ab3eeb
Kyle K <doctor.whom@gmail.com>
2019-07-27 09:40:46 +0000

    barely works now ^_^

--- console.js
+++ console.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//var serial = navigator.serial;
-//reader.read().then(function(fij) { asdf=fij;let textDecoder = new TextDecoder(); t.io.print(textDecoder.decode(fij.value.buffer)); });
 hterm.defaultStorage = new lib.Storage.Local();
 
 var port;
@@ -12,99 +10,80 @@ let textEncoder = new TextEncoder();
 
 let t = new hterm.Terminal();
 t.onTerminalReady = () => {
-console.log('Terminal ready.');
-let io = t.io.push();
+  console.log('Terminal ready.');
+  t.io.println('This currently requires a fairly recent build of chrome with a particular flag enabled');
+  t.io.println('Try Chrome 77+ Canary, possibly Dev channel');
+  t.io.println('For a betaflight flight controller, click connect, follow through and then type #');
+  //t.io.println('chrome://flags/#enable-experimental-web-platform-features');
+  let io = t.io.push();
 
-io.onVTKeystroke = str => {
-    if (port.writable !== undefined) {
+  io.onVTKeystroke = str => {
+    if (port && port.writable !== undefined) {
         writer.write(textEncoder.encode(str));
         //port.send(textEncoder.encode(str)).catch(error => {
         //  t.io.println('Send error: ' + error);
         //});
     }
-};
-
-    io.sendString = str => {
-      if (port.writable !== undefined) {
-        port.writable.write(str);
-        //port.send(textEncoder.encode(str)).catch(error => {
-        //  t.io.println('Send error: ' + error);
-        //});
-      }
-    };
   };
 
-  document.addEventListener('DOMContentLoaded', event => {
-    let connectButton = document.querySelector('#connect');
+  io.sendString = str => {
+    if (port.writable !== undefined) {
+      writer.write(textEncoder.encode(str));
+      //port.send(textEncoder.encode(str)).catch(error => {
+      //  t.io.println('Send error: ' + error);
+      //});
+    }
+  };
+};
 
-    t.decorate(document.querySelector('#terminal'));
-    t.setWidth(80);
-    t.setHeight(24);
-    t.installKeyboard();
+document.addEventListener('DOMContentLoaded', event => {
+  let connectButton = document.querySelector('#connect');
 
-    function connect() {
-      t.io.println('Connecting to ' + port.device_.productName + '...');
-      port.open().then(() => {
-        console.log(port);
-        t.io.println('Connected.');
-        connectButton.textContent = 'Disconnect';
-        port.onReceive = data => {
+  t.decorate(document.querySelector('#terminal'));
+  t.setWidth(100);
+  t.setHeight(24);
+  t.installKeyboard();
+
+  function connect() {
+    t.io.println('Connecting to ' + port.device_.productName + '...');
+    port.open().then(() => {
+      console.log(port);
+      t.io.println('Connected.');
+      connectButton.textContent = 'Disconnect';
+      port.onReceive = data => {
+        let textDecoder = new TextDecoder();
+        t.io.print(textDecoder.decode(data));
+      }
+      port.onReceiveError = error => {
+        t.io.println('Receive error: ' + error);
+      };
+    }, error => {
+      t.io.println('Connection error: ' + error);
+    });
+  };
+
+  connectButton.addEventListener('click', async function() {
+      //console.log('hello there');
+      // Will this loop cause a stack overflow?
+      let readLoop = () => {
+        reader.read().then(value => {
+          //console.log(value);
           let textDecoder = new TextDecoder();
-          t.io.print(textDecoder.decode(data));
-        }
-        port.onReceiveError = error => {
-          t.io.println('Receive error: ' + error);
-        };
-      }, error => {
-        t.io.println('Connection error: ' + error);
-      });
-    };
-
-    connectButton.addEventListener('click', async function() {
-        console.log('hello there');
-        let readLoop = () => {
-            reader.read().then(value => {
-                console.log(value);
-                let textDecoder = new TextDecoder();
-                t.io.print(textDecoder.decode(value.value.buffer)); 
-                readLoop();
-                }, error => {console.log(error);});
-        };
-        try {
-            port = await navigator.serial.requestPort({});
-            await port.open({baudrate: 115200 })
-            writer = port.writable.getWriter();
-            reader = port.readable.getReader();
-            readLoop();
-            console.log(port);
-        } catch (error) {
-            console.error(error);
-        }
-        /*
-      if (port) {
-        port.disconnect();
-        connectButton.textContent = 'Connect';
-        port = null;
-      } else {
-        serial.requestPort({baudrate: 115200}).then(selectedPort => {
-console.log(selectedPort);
-          port = selectedPort;
-          connect();
-        }).catch(error => {
-          t.io.println('Connection error: ' + error);
-        });
+          t.io.print(textDecoder.decode(value.value.buffer)); 
+          readLoop();
+        }, error => {console.log(error);});
+      };
+      try {
+          port = await navigator.serial.requestPort({});
+          // help command requires more than 3000 bytes 4096 8192
+          await port.open({baudrate: 115200, buffersize: 8192 })
+          writer = port.writable.getWriter();
+          reader = port.readable.getReader();
+          readLoop();
+          //console.log(port);
+      } catch (error) {
+          console.error(error);
       }
-      */
-    });
-/*
-    serial.getPorts().then(ports => {
-      if (ports.length == 0) {
-        t.io.println('No devices found.');
-      } else {
-        port = ports[0];
-        connect();
-      }
-    });
-*/
   });
+});
 
